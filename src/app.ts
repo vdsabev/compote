55fd68074e81d6a590551fce11c65af8cbfe43d3
Vladimir Sabev <vdsabev@gmail.com>
2016-12-26 05:29:22 +0200

    Added HTML tag functions

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,26 @@
 module compote.app {
-  const { $, bind, Component } = core;
+  const { HTML, bind, Component } = core;
+  const { div, input, br, img, hr, button } = HTML;
 
   /** App */
   export class AppComponent extends Component {
-    $render(): core.ComponentTree {
-      return [
-        `div.app(title="${this.name}")`, {}, [
-          $(`input(type="text" value="${this.name}" onInput="${this.setName()}")`),
-          $(`br`),
+    $render() {
+      return (
+        div({ class: `app`, title: this.name }, [
+          input({ type: `text`, value: this.name, onInput: this.setName() }),
+          br(),
           `Text node: ${this.name}`,
-          $(`br`),
-          $(`img(alt="Element attribute: ${this.name}")`),
-          $(`div`, {}, [`Element content: ${this.name}`]),
+          br(),
+          img({ alt: `Element attribute: ${this.name}` }),
+          div({}, `Element content: ${this.name}`),
           // TODO: Support merging definition / children
-          Label(`span.todo`, { text: `Custom component: ${this.name}` }),
+          Label({ class: 'todo', data: { text: `Custom component: ${this.name}` } }),
 
-          $(`hr`),
-          $(`button(type="button" onClick="${this.incrementCounter()}")`, {}, [`Count me in!`]),
+          hr(),
+          button({ type: 'button', onClick: this.incrementCounter() }, `Count me in!`),
           ` Button clicked ${this.counter} times`
-        ]
-      ];
+        ])
+      );
     }
 
     @bind name = `Alice`;
@@ -37,13 +38,13 @@ module compote.app {
   }
 
   /** Label */
-  export function Label(definition = ``, data: core.ComponentData<LabelComponent> = {}) {
-    return new LabelComponent(definition, data);
+  export function Label(attributes?: core.ComponentAttributes<LabelComponent>): core.ComponentTree {
+    return [Object.assign({ Component: LabelComponent }, attributes), []];
   }
 
   export class LabelComponent extends Component {
     $render(): core.ComponentTree {
-      return [`div`, {}, [this.text]];
+      return div({}, this.text);
     }
 
     @bind text: string;
