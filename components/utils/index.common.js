6df37113b1574cfc7322b4b1b95a554b6630663b
Vladimir Sabev <vdsabev@gmail.com>
2017-05-05 17:05:45 +0300

    Added set & setFlag; changed toEqual to toBe where applicable

--- components/utils/index.common.js
+++ components/utils/index.common.js
@@ -4,6 +4,23 @@ function get(propertyName) {
     return function (obj) { return obj[propertyName]; };
 }
 exports.get = get;
+function set(propertyName) {
+    return function (obj) { return function (value) { return obj[propertyName] = value; }; };
+}
+exports.set = set;
+function setFlag(obj, propertyName, newValue) {
+    if (newValue === void 0) { newValue = true; }
+    var originalValue = obj[propertyName];
+    obj[propertyName] = newValue;
+    return {
+        whileAwaiting: function (promise) {
+            var unsetFlag = function () { return obj[propertyName] = originalValue; };
+            // We could use `finally`, but some promises (e.g. Firebase) don't support it
+            return promise["catch"](unsetFlag).then(unsetFlag);
+        }
+    };
+}
+exports.setFlag = setFlag;
 function groupBy(propertyName) {
     var valueOfProperty = get(propertyName);
     return function (items) {
