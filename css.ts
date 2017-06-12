21c93601d458ce7f968c81abd8b2ae2bb9e7f69e
Vladimir Sabev <vdsabev@gmail.com>
2017-06-12 19:54:59 +0300

    Changed type of getAnimationDuration argument to Element

--- css.ts
+++ css.ts
@@ -1,6 +1,6 @@
 import { ComponentNode } from './html';
 
-export const getAnimationDuration = ($el: HTMLElement) => parseFloat(window.getComputedStyle($el).animationDuration);
+export const getAnimationDuration = ($el: Element) => parseFloat(window.getComputedStyle($el).animationDuration);
 
 export const setAnimation = (animationClass: string, timingScale = 0.95) => ({ dom }: ComponentNode) => {
   dom.classList.add(animationClass);
