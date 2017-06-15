7a6a27e1bed3141f2e8159a0894c751d68387ad7
Vladimir Sabev <vdsabev@gmail.com>
2017-06-15 15:30:57 +0300

    Changed second setAnimation parameter to duration

--- css.ts
+++ css.ts
@@ -2,9 +2,9 @@ import { ComponentNode } from './html';
 
 export const getAnimationDuration = ($el: Element) => parseFloat(window.getComputedStyle($el).animationDuration);
 
-export const setAnimation = (animationClass: string, timingScale = 0.95) => ({ dom }: ComponentNode) => {
+export const setAnimation = (animationClass: string, duration?: number) => ({ dom }: ComponentNode) => {
   dom.classList.add(animationClass);
   return new Promise((resolve) => {
-    setTimeout(resolve, timingScale * getAnimationDuration(dom) * 1e3);
+    setTimeout(resolve, duration == null ? 0.95 * getAnimationDuration(dom) * 1e3 : duration);
   });
 };
